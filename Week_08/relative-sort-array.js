954a05cd4c9a0680b6138cb4b33b4d5b93126e5d
Lincoln <herowalking@hotmail.com>
2020-08-14 17:08:18 +0800

    add relative-sort-array.js && reverse-bits.js

---
+++ Week_08/relative-sort-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
+ */
+var relativeSortArray = function(arr1, arr2) {
+  return arr1.sort((a, b) => {
+    let indexA = arr2.indexOf(a);
+    let indexB = arr2.indexOf(b);
+    if (indexA === -1 && indexB === -1) {
+      return a - b
+    } else if (indexA === -1) {
+      return 1;
+    } else if (indexA !== -1 && indexB !== -1) {
+      return indexA - indexB;
+    }
+  })
+};
